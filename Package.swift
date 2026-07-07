d37b29d3f24f5c32767b6ef5ed08fab1e1eb06b0
ci <ci@banuba.com>
2026-07-07 10:49:32 +0300

    1.18.3

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.18.2-24-g50ecbc89fd"
+let bnbPackageVersion: Version = "1.18.3"
 
 let package = Package(
     name: "BNBFaceMatch",
@@ -31,8 +31,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBFaceMatch",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.18.2-24-g50ecbc89fd/BNBFaceMatch.zip",
-            checksum: "b2cbd739f736536b79e3de0135429872fe95560f1db3590ae1042d3ef663b9db"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.18.3/BNBFaceMatch.zip",
+            checksum: "3b12f69c0a851afc30d977b78838cb031d85e0a7e182ce5afbde92c0e11e6f83"
         ),
         .target(
             name: "BNBFaceMatch_BNBSdkCore",
